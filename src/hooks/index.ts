30be58983d0a917f8cd2ffd3abb3d31aa3199d1f
Hssan <finova.front@gmail.com>
2024-02-27 13:59:53 +0200

    new

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,11 @@
 import cookies from "js-cookie";
 
-export const Url :string = "http://192.168.1.10:8000"
-export const Url_img :string = "http://192.168.1.10:8000"
+// export const Url :string = "http://192.168.1.10:8000"
+// export const Url_img :string = "http://192.168.1.10:8000"
 //export const Url :string = "https://finovafintech.io/deltaagro/admincms"
 // export const Url_img :string = "https://finovafintech.io"
-// export const Url :string = "./admincms"
-// export const Url_img :string = ".."
+export const Url :string = "./admincms"
+export const Url_img :string = ".."
 export const en :string = "api"
 export const fr :string = "api/fr"
 
