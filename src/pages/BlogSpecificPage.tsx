74a4e75e87505456fb2c8167cd6c4ef5e716da16
Hasan Naim <finova.front@gmail.com>
2024-07-23 12:46:12 +0300

    new

--- src/pages/BlogSpecificPage.tsx
+++ src/pages/BlogSpecificPage.tsx
@@ -1,14 +1,30 @@
-import React from "react";
 import { useParams } from "react-router-dom";
-
+import Loader from "../components/Loader";
+import { lng, Url } from "../hooks";
+import { useQuery } from "@tanstack/react-query";
+//@ts-ignore
+import DOMPurify from "dompurify";
 const BlogSpecificPage = () => {
   const { id } = useParams();
-  console.log(id)
-  return (
-    <div>
-        
-    </div>
-  )
+  const { isFetching, error, data } = useQuery({
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    queryKey: ["blogs", id],
+    queryFn: () => fetch(`${Url}/${lng}/blog/${id}`).then((res) => res.json()),
+  });
+  if (isFetching) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <div>An error has occurred: {error.message}</div>;
+  }
+
+  const sanitizedDescription = data
+    ? DOMPurify.sanitize(data.description || "")
+    : "";
+
+  return <div dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />;
 };
 
 export default BlogSpecificPage;
